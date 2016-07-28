72eb421305fdfe6c8c647ba52cf6a13d406e4507
Suresh Shinde <suresh.socialchamps@gmail.com>
2016-07-28 18:54:19 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -65,13 +65,7 @@ var read = function (sender, message, reply) {
 			}
 		})
 	}
-	if(message === 'Send me images' || message === 'project images'){
-		var reply = 
-    new builder.Message()
-        .setText(session, text)
-        .addAttachment({ fallbackText: text, contentType: 'image/jpeg', contentUrl: 'http://sunshinehills.in/img/gallery/1.jpg' });
-		session.send(reply);
-	}
+
 }
 
 
